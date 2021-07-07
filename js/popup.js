156740480b2a4d8be398dd431a2d97a3aab80ab2
memerde <44968588+memerde@users.noreply.github.com>
2021-07-07 21:59:59 +0300

    упд

--- js/popup.js
+++ js/popup.js
@@ -2,7 +2,6 @@ const popBtn = document.querySelector(".popup-btn");
 const formIndex = document.querySelector(".form-index");
 const dayIn = document.querySelector(".input-day-in");
 
-
 popBtn.addEventListener("click", function () {
   formIndex.classList.toggle("modal-show");
   dayIn.focus();
