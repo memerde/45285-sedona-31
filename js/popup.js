2faa5c7ecf2ec36c7a8f06271957b2ead02e9e21
memerde <44968588+memerde@users.noreply.github.com>
2021-07-16 23:06:55 +0300

    упд анима

--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,22 @@
 const popBtn = document.querySelector(".popup-btn");
 const formIndex = document.querySelector(".form-index");
 const dayIn = document.querySelector(".input-day-in");
+const dayOut = document.querySelector(".input-day-out");
+const adult = document.querySelector(".adult-input");
+const child = document.querySelector(".child-input");
 
 popBtn.addEventListener("click", function () {
   formIndex.classList.toggle("modal-show");
-  dayIn.focus();
+});
+
+formIndex.addEventListener("submit", function (evt) {
+  if (!dayIn.value || !dayOut.value || !adult.value || !child.value) {
+    evt.preventDefault();
+    formIndex.classList.toggle("modal-error");
+  } else {
+    dayIn.setItem("in", dayIn.value);
+    dayOut.setItem("out", dayOut.value);
+    adult.setItem("adult", adult.value);
+    child.setItem("child", child.value);
+  }
 });
